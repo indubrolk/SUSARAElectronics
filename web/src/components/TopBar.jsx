4752b930f87d1caefd0042be88b1f3d97b33a239
Indu <indubro.lk@gmail.com>
2026-06-07 10:28:45 +0530

    social links added

--- web/src/components/TopBar.jsx
+++ web/src/components/TopBar.jsx
@@ -13,19 +13,19 @@ export default function TopBar() {
                         </div>
                         <div className="flex items-center space-x-2">
                             <MapPin className="w-4 h-4 text-yellow-500" />
-                            <span>Bamunakotuwa, Wariyapola, Sri Lanka</span>
+                            <span>Kalagaha, Panadaragama, Wariyapola, Sri Lanka</span>
                         </div>
                     </div>
 
                     {/* Right side: Social Icons */}
                     <div className="flex space-x-4 text-gray-800">
-                        <a href="#" className="bg-black-400 hover:text-yellow-500 transition-colors">
+                        <a href="https://web.facebook.com/susaraelectronics.lk" className="bg-black-400 hover:text-yellow-500 transition-colors">
                             <Facebook className="w-4 h-4" />
                         </a>
-                        <a href="#" className="hover:text-yellow-500 transition-colors">
+                        <a href="https://www.instagram.com/susara_electronics/" className="hover:text-yellow-500 transition-colors">
                             <Instagram className="w-4 h-4" />
                         </a>
-                        <a href="#" className="hover:text-yellow-500 transition-colors">
+                        <a href="https://x.com/susaraelectron1" className="hover:text-yellow-500 transition-colors">
                             <Twitter className="w-4 h-4" />
                         </a>
                     </div>
